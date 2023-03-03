a8cb4e332539f8064920574a0548ad9afb78dc6a
Roman Degtyarev <romadegtyarev68y@gmail.com>
2023-03-03 12:27:49 +0500

    Chicha and Chich

--- zaepet.py
+++ zaepet.py
@@ -2,7 +2,7 @@ num = int(input('Введите 1 или 2 или 3: '))
 if num == 1:
     print('Лошадь')
     print('корова')
-    print('говнарь Чича')
+    print('говнарь Чич')
 elif num == 3:
     print('свин - Пеп')
 else:
