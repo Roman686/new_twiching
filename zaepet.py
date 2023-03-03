9d7320900472e37afa2d070eb30e5f4c5a99f90e
Roman686 <60396702+Roman686@users.noreply.github.com>
2023-03-03 10:20:50 +0500

    Update zaepet.py

--- zaepet.py
+++ zaepet.py
@@ -1,8 +1,10 @@
-num = int(input('Введите 1 или 2: '))
+num = int(input('Введите 1 или 2 или 3: '))
 if num == 1:
     print('Лошадь')
     print('корова')
     print('говнарь Чича')
+elif num == 3:
+    print('свин - Пеп')
 else:
     print('Хеллло!')
     print('говнарь Боба')
